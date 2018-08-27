d3d6a97d9be3a38395dee0369b047f3bb6e823b2
Ankit <ankit.joshi407@gmail.com>
2018-08-27 22:40:31 +0530

    frist commit

---
+++ finalkeyword.java
@@ -0,0 +1,14 @@
+final class test
+{
+  public static void abc()
+  {
+	  System.out.println("we cannot inherit class if we use final keyword");
+  }
+}
+public class finalkeyword
+{
+	public static void main(String[] args)
+	{
+		test.abc();
+	}
+}
